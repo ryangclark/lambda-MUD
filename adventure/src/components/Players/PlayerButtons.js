504d974186f25072748c1f7541ca6d3d1a50acf8
Clark <rclark010@gmail.com>
2019-08-01 12:53:52 -0600

    Initial Commit

---
+++ adventure/src/components/Players/PlayerButtons.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Players.css';
+
+const PlayerButtons = props => {
+	let exits = (props.players[props.activeToken] && 
+		props.players[props.activeToken].room.exits) ||
+		{}
+
+	// console.log('exits', exits)
+
+	return (
+		<div className="player-buttons">
+			{['n', 's', 'e', 'w'].map(i => (
+				<button
+					className="move-button"
+					disabled={!exits.hasOwnProperty(i)}
+					key={i}
+					onClick={event => {
+						event.preventDefault()
+						props.movePlayer(
+							i,
+							props.players[props.activeToken].room,
+							props.activeToken
+						)
+					}}
+				>{i}</button>
+			))}
+			
+			
+		</div>
+	)
+}
+
+// <button disabled={!exits.hasOwnProperty('s')}>S</button>
+// <button disabled={!exits.hasOwnProperty('e')}>E</button>
+// <button disabled={!exits.hasOwnProperty('w')}>W</button>
+
+export default PlayerButtons
